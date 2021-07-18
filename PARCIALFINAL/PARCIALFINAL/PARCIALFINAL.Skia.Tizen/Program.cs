b1b2f714cc2bafd6037ef5ff6943067b3e30f97d
SANTIAGO MESA RESTREPO <santiagowe1529@gmail.com>
2021-07-18 00:20:20 -0500

    Login

---
+++ PARCIALFINAL/PARCIALFINAL/PARCIALFINAL.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace PARCIALFINAL.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new PARCIALFINAL.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ PARCIALFINAL/PARCIALFINAL/PARCIALFINAL.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace PARCIALFINAL.Skia.Gtk
+{
+}
